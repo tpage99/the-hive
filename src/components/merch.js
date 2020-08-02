f69091636b91eea5972bc68779e8901c98883b19
Taylor Page <taylor@thepagesmedia.com>
2020-08-02 09:29:37 -0500

    updated for componentDidMount for scripts and changed to class component

--- src/components/merch.js
+++ src/components/merch.js
@@ -1,27 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
 
-function Merch() {
-  window.localStorage.setItem("show_ecwid_logs", "true")
-  window.ecwid_script_defer = true
-  window.ecwid_dynamic_widgets = true
-  window.ec = window.ec || Object()
-  window.ec.storefront = window.ec.storefront || Object()
-  window.ec.enable_catalog_on_one_page = true
+class Merch extends Component {
+  componentDidMount() {
+    window.localStorage.setItem("show_ecwid_logs", "true")
+    window.ecwid_script_defer = true
+    window.ecwid_dynamic_widgets = true
+    window.ec = window.ec || Object()
+    window.ec.storefront = window.ec.storefront || Object()
+    window.ec.enable_catalog_on_one_page = true
 
-  var script = document.createElement("script")
-  script.charset = "utf-8"
-  script.type = "text/javascript"
-  script.src = "https://app.ecwid.com/script.js?34089289"
-  script.defer = true
+    var script = document.createElement("script")
+    script.charset = "utf-8"
+    script.type = "text/javascript"
+    script.src = "https://app.ecwid.com/script.js?34089289"
+    script.defer = true
+  }
 
-  return (
-    <div>
-      <div id="my-search-34089289"></div>
-      <div id="my-categories-34089289"></div>
-      <div id="my-store-34089289"></div>
-      <div className="ec-cart-widget"></div>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <div id="my-search-34089289"></div>
+        <div id="my-categories-34089289"></div>
+        <div id="my-store-34089289"></div>
+        <div className="ec-cart-widget"></div>
+      </div>
+    )
+  }
 }
 
 export default Merch
